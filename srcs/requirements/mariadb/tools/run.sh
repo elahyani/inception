298b1cac0dfc1e69898e1fd62628d02d72cdffe6
elahyani <hassanlhyani7@gmail.com>
2021-10-23 18:39:39 +0100

    set wordpress database and fix mriadb ,new users issue

--- srcs/requirements/mariadb/tools/run.sh
+++ srcs/requirements/mariadb/tools/run.sh
@@ -3,15 +3,13 @@
 if [ ! -f "/var/lib/mysql/ib_buffer_pool" ]; then
 	/etc/init.d/mariadb setup
 	/etc/init.d/mariadb start
-	mysql -u root -e "flush privileges"
-	mysql -u root -e "create user 'elahyani'@'%' identified by 'bIJ2rwW'"
-	mysql -u root -e "create database wordpress"
-	mysql -u root -e "grant all privileges on *.* to 'elahyani'@'%' identified by 'bIJ2rwW'"
-	mysql -u root -e "flush privileges"
-	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'slhfdfj'"
-	mysql -u root -p slhfdfj "flush privileges"
+	mysql -u root -e "DELETE FROM mysql.user WHERE User=''; FLUSH PRIVILEGES;"
+	mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+	mysql -u root -e "CREATE DATABASE ${MYSQL_DATABASE};"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* to '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; FLUSH PRIVILEGES;"
+	mysql -u root wordpress < /wordpress.sql
+	mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
 	/etc/init.d/mariadb stop
-	# mysql -u root < /wordpress.sql
 fi
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 sed -i "s|.*bind-address\s*=.*|bind-address=0.0.0.0|g" /etc/my.cnf.d/mariadb-server.cnf
