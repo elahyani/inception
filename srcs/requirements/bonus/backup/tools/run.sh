e0701904081515cd6349236391acd7090a9eb7c1
elahyani <hassanlhyani7@gmail.com>
2021-10-29 19:47:41 +0100

    fix wrong path for backup service

--- srcs/requirements/bonus/backup/tools/run.sh
+++ srcs/requirements/bonus/backup/tools/run.sh
@@ -6,6 +6,6 @@ touch /backup/backup_history.txt
 
 echo "data backed up - [`date`]" >> /backup/backup_history.txt
 mkdir /backup/wp_backup/wp-bkp$curr_date
-cp -rf /www/wordpress/* /backup/wp_backup/wp-bkp
+cp -rf /www/wordpress/* /backup/wp_backup/wp-bkp$curr_date
 mkdir /backup/db_backup/db-bkp$curr_date
-cp -rf /var/lib/mysql/* /backup/db_backup/db-bkp
+cp -rf /var/lib/mysql/* /backup/db_backup/db-bkp$curr_date
